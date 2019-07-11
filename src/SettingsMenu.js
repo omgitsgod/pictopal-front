165dc69b7c6d54306bc2c0a42499313e5c0b5da6
omgitsgod <omgitsgod@gmail.com>
2019-07-10 23:49:39 -0400

    Clean up props and imports

--- src/SettingsMenu.js
+++ src/SettingsMenu.js
@@ -1,9 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import {SketchField, Tools} from 'react-sketch';
 import Icon from '@mdi/react'
-import GoogleButton from 'react-google-button'
-import {mdiContentSave, mdiRecord, mdiCctv, mdiTrashCanOutline, mdiShareOutline, mdiSettingsOutline, mdiUndo, mdiRedo, mdiArrowSplitHorizontal, mdiNewBox, mdiAccountCircle, mdiPencil, mdiChevronDoubleRight, mdiRayStartArrow, mdiDragVertical, mdiRectangleOutline, mdiCircleOutline, mdiPan, mdiPalette} from '@mdi/js'
-import { SliderPicker } from 'react-color';
+import {mdiContentSave, mdiCctv, mdiTrashCanOutline, mdiShareOutline} from '@mdi/js'
 import ReactTooltip from 'react-tooltip'
 import { isMobile } from "react-device-detect"
 import './App.css';
--- src/SketchTools.js
+++ src/SketchTools.js
@@ -1,9 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import {SketchField, Tools} from 'react-sketch';
+import {Tools} from 'react-sketch';
 import Icon from '@mdi/react'
-import GoogleButton from 'react-google-button'
-import {mdiContentSave, mdiRecord, mdiCctv, mdiTrashCanOutline, mdiShareOutline, mdiSettingsOutline, mdiUndo, mdiRedo, mdiArrowSplitHorizontal, mdiNewBox, mdiAccountCircle, mdiPencil, mdiChevronDoubleRight, mdiRayStartArrow, mdiDragVertical, mdiRectangleOutline, mdiCircleOutline, mdiPan, mdiPalette} from '@mdi/js'
-import { SliderPicker } from 'react-color';
+import {mdiUndo, mdiRedo, mdiArrowSplitHorizontal, mdiNewBox, mdiPencil, mdiChevronDoubleRight, mdiRayStartArrow, mdiRectangleOutline, mdiCircleOutline, mdiPan, mdiPalette} from '@mdi/js'
 import ReactTooltip from 'react-tooltip'
 import { isMobile } from "react-device-detect"
 import './App.css';
