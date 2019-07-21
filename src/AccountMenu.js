081b26910428164cf15740a4f64f78cb5fd2bf86
omgitsgod <omgitsgod@gmail.com>
2019-07-20 23:09:49 -0400

    Add test button

--- src/AccountMenu.js
+++ src/AccountMenu.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import Icon from '@mdi/react'
 import GoogleButton from 'react-google-button'
-import {mdiContentSave, mdiLogout, mdiAccountBox, mdiRecord, mdiCctv, mdiTrashCanOutline, mdiShareOutline, mdiSettingsOutline, mdiUndo, mdiRedo, mdiArrowSplitHorizontal, mdiNewBox, mdiAccountCircle, mdiPencil, mdiChevronDoubleRight, mdiRayStartArrow, mdiDragVertical, mdiRectangleOutline, mdiCircleOutline, mdiPan, mdiPalette} from '@mdi/js'
+import {mdiContentSave, mdiTestTube, mdiLogout, mdiAccountBox, mdiRecord, mdiCctv, mdiTrashCanOutline, mdiShareOutline, mdiSettingsOutline, mdiUndo, mdiRedo, mdiArrowSplitHorizontal, mdiNewBox, mdiAccountCircle, mdiPencil, mdiChevronDoubleRight, mdiRayStartArrow, mdiDragVertical, mdiRectangleOutline, mdiCircleOutline, mdiPan, mdiPalette} from '@mdi/js'
 import ReactTooltip from 'react-tooltip'
 import { isMobile } from "react-device-detect"
 import './App.css';
@@ -11,6 +11,11 @@ function AccountMenu(props) {
     <div className="accountmenu">
     {props.isAuthenticated ?
       <div>
+      <Icon path={mdiTestTube}
+        size={2}
+        color="#92a3a8"
+        onClick={()=>fetch('https://pictopal-backend.herokuapp.com/test')}
+      />
       <Icon path={mdiAccountBox}
         size={2}
         color="#92a3a8"
