c7689b517e500823770a9f7346760fdc49cb298f
omgitsgod <omgitsgod@gmail.com>
2019-07-22 21:45:38 -0400

    Change icon of destuct

--- src/AccountMenu.js
+++ src/AccountMenu.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import Icon from '@mdi/react'
 import GoogleButton from 'react-google-button'
-import {mdiContentSave, mdiClose, mdiTestTube, mdiLogout, mdiAccountBox, mdiRecord, mdiCctv, mdiTrashCanOutline, mdiShareOutline, mdiSettingsOutline, mdiUndo, mdiRedo, mdiArrowSplitHorizontal, mdiNewBox, mdiAccountCircle, mdiPencil, mdiChevronDoubleRight, mdiRayStartArrow, mdiDragVertical, mdiRectangleOutline, mdiCircleOutline, mdiPan, mdiPalette} from '@mdi/js'
+import {mdiContentSave, mdiNuke, mdiTestTube, mdiLogout, mdiAccountBox, mdiRecord, mdiCctv, mdiTrashCanOutline, mdiShareOutline, mdiSettingsOutline, mdiUndo, mdiRedo, mdiArrowSplitHorizontal, mdiNewBox, mdiAccountCircle, mdiPencil, mdiChevronDoubleRight, mdiRayStartArrow, mdiDragVertical, mdiRectangleOutline, mdiCircleOutline, mdiPan, mdiPalette} from '@mdi/js'
 import ReactTooltip from 'react-tooltip'
 import { isMobile } from "react-device-detect"
 import './App.css';
@@ -11,7 +11,7 @@ function AccountMenu(props) {
     <div className="accountmenu">
     {props.isAuthenticated ?
       <div>
-      <Icon path={mdiClose}
+      <Icon path={mdiNuke}
         size={2}
         color="#92a3a8"
         onClick={()=>fetch('https://pictopal-backend.herokuapp.com/close',{method: 'GET',
