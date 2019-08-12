c14758abc3684170129bf6a676c7b6fc9653a29a
omgitsgod <omgitsgod@gmail.com>
2019-08-11 21:58:04 -0400

    Add settings menu to account menu

--- src/AccountMenu.js
+++ src/AccountMenu.js
@@ -1,16 +1,23 @@
 import React, {useState, useEffect} from 'react';
 import Icon from '@mdi/react'
 import GoogleButton from 'react-google-button'
+import SettingsMenu from './SettingsMenu'
 import {mdiContentSave, mdiNuke, mdiTestTube, mdiLogout, mdiAccountBox, mdiRecord, mdiCctv, mdiTrashCanOutline, mdiShareOutline, mdiSettingsOutline, mdiUndo, mdiRedo, mdiArrowSplitHorizontal, mdiNewBox, mdiAccountCircle, mdiPencil, mdiChevronDoubleRight, mdiRayStartArrow, mdiDragVertical, mdiRectangleOutline, mdiCircleOutline, mdiPan, mdiPalette} from '@mdi/js'
 import ReactTooltip from 'react-tooltip'
 import { isMobile } from "react-device-detect"
 import './App.css';
 
 function AccountMenu(props) {
+  const [settingsMenu, setSettingsMenu] = useState(false)
   return (
     <div className="accountmenu">
     {props.isAuthenticated ?
       <div>
+      {settingsMenu ?
+        <SettingsMenu reff={props.reff} handleLive={props.handleLive}/>
+        :
+        null
+      }
       <Icon path={mdiTestTube}
         size={2}
         color="#92a3a8"
