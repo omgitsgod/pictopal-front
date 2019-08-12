a03ad34ac70b09cacd6322ca4f3eb288aac9fe6f
omgitsgod <omgitsgod@gmail.com>
2019-08-11 22:00:03 -0400

    Add settings icon

--- src/AccountMenu.js
+++ src/AccountMenu.js
@@ -18,6 +18,13 @@ function AccountMenu(props) {
         :
         null
       }
+      <Icon path={mdiSettingsOutline}
+        className="bottomrighticon"
+        size={2}
+        color="#92a3a8"
+        onClick={()=>setSettingsMenu(!settingsMenu)}
+        data-tip data-for='settings'
+      />
       <Icon path={mdiTestTube}
         size={2}
         color="#92a3a8"
