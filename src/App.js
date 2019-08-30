6c1ebe909170a7cfc213e70925f8e827b32cd9e4
omgitsgod <omgitsgod@gmail.com>
2019-08-30 18:15:20 -0400

    Add log of objects

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ credentials: 'include'})
       connection.onmessage = (e) => {
         console.log(e.data)
         let objects = JSON.parse(e.data)
-        console.log(objects);
+        console.log(objects.objects);
         // props.reff.fromJSON(e.data)
       //   setValues(objects)
       }
